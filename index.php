078f66baf14ebbfc968c2cab5656f93d65363441
Denys Skotarenko <skotarenkodenis@gmail.com>
2020-12-13 14:48:27 +0200

    added basic structure of HTML

--- index.php
+++ index.php
@@ -1,1 +1,89 @@
-<?php
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная страница</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo_wrapper">
+            <h1><a href="/" class="logo">IT<span>blog</span></a></h1>
+        </div>
+        <!-- logo_wrapper -->
+            <nav class="main_nav">
+                <ul class="nav_list">
+                    <li class="nav_list_item"><a href="#">HTML & CSS</a></li>
+                    <li class="nav_list_item"><a href="#">JavaScript</a></li>
+                    <li class="nav_list_item"><a href="#">PHP</a></li>
+                    <li class="nav_list_item"><a href="#">О проекте</a></li>
+                    <li class="nav_list_item"><a href="#">Вход</a></li>
+                </ul>
+            </nav>
+            <!-- main_nav -->
+    </header>
+<main class="main_wrapper">
+    <section class="slider">
+        <h2 class="visually_hidden sub_title">Slider</h2>
+        <ul class="slider_list">
+            <li class="slider_item"><img src="/images/arch_linux.png" alt="" width="1440" height="900"></li>
+        </ul>
+    </section>
+    <!-- slider -->
+    <section class="about_project">
+        <h2 class="sub_title">О проекте</h2>
+        <div class="project_content">
+            Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. Я совсем один и блаженствую в здешнем краю, словно созданном для таких, как я. Я так счастлив, мой друг, так упоен ощущением покоя, что искусство мое страдает от этого. Ни одного штриха не мог бы я сделать, а никогда не был таким большим художником, как в эти минуты. Когда от милой моей долины поднимается пар и полдневное солнце стоит над непроницаемой чащей темного леса и лишь редкий луч проскальзывает в его святая святых, а я лежу в высокой траве у быстрого ручья и, прильнув к земле, вижу тысячи всевозможных былинок и чувствую, как близок моему сердцу крошечный мирок, что снует между стебельками, наблюдаю эти неисчислимые, непостижимые разновидности червяков и мошек и чувствую близость всемогущего, создавшего нас по своему подобию, веяние вселюбящего, судившего нам парить в вечном блаженстве, когда взор мой туманится и все вокруг меня и небо надо мной запечатлены в моей душе, точно образ возлюбленной, - тогда, дорогой друг, меня часто томит мысль: "Ах! Как бы выразить, как бы вдохнуть в рисунок то, что так полно, так трепетно живет во мне, запечатлеть отражение моей души, как душа моя - отражение предвечного бога!" Друг
+        </div>
+    </section>
+    <section class="popular_articles">
+        <h2>Популярные статьи</h2>
+        <div class="popular_article_wrapper">
+            <article class="popular_article">
+                <h3><a href="#">Название стать</a></h3>
+                <img src="/images/popular_article.jpg" alt="" width="304" height="176">
+            </article>
+            <article class="popular_article">
+                <h3><a href="#">Название стать</a></h3>
+                <img src="/images/popular_article.jpg" alt="" width="304" height="176">
+            </article>
+            <article class="popular_article">
+                <h3><a href="#">Название стать</a></h3>
+                <img src="/images/popular_article.jpg" alt="" width="304" height="176">
+            </article>
+            <article class="popular_article">
+                <h3><a href="#">Название стать</a></h3>
+                <img src="/images/popular_article.jpg" alt="" width="304" height="176">
+            </article>
+            <article class="popular_article">
+                <h3><a href="#">Название стать</a></h3>
+                <img src="/images/popular_article.jpg" alt="" width="304" height="176">
+            </article>
+        </div>
+        <!-- popular_article_wrapper -->
+    </section>
+</main>
+<!-- main_wrapper -->
+<footer class="footer">
+    <div class="logo_wrapper logo_wrapper_footer">
+        <h1><a href="/" class="logo logo_footer">IT<span>blog</span></a></h1>
+    </div>
+    <section class="social">
+        <h2 class="visually_hidden">Мы в социальных сетях</h2>
+        <ul class="social_list">
+            <li class="social_icon">
+                <a href="#">Twitter</a>
+            </li>
+            <li class="social_icon">
+                <a href="#">Facebook</a>
+            </li>
+            <li class="social_icon">
+                <a href="#">youtube</a>
+            </li>
+        </ul>
+    </section>
+</footer>
+</body>
+</html>
