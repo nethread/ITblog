dee3d481c19d15d8b4035511f15fc12b32eda858
Denys Skotarenko <skotarenkodenis@gmail.com>
2020-12-15 20:49:14 +0200

    added styles for index.php

--- index.php
+++ index.php
@@ -11,10 +11,9 @@
 <body>
     <header class="header">
         <div class="logo_wrapper">
-            <h1><a href="/" class="logo">IT<span>blog</span></a></h1>
+            <h1><a href="/" class="logo">IT.blog</a></h1>
         </div>
         <!-- logo_wrapper -->
-            <nav class="main_nav">
                 <ul class="nav_list">
                     <li class="nav_list_item"><a href="#">HTML & CSS</a></li>
                     <li class="nav_list_item"><a href="#">JavaScript</a></li>
@@ -22,12 +21,11 @@
                     <li class="nav_list_item"><a href="#">О проекте</a></li>
                     <li class="nav_list_item"><a href="#">Вход</a></li>
                 </ul>
-            </nav>
-            <!-- main_nav -->
+            <!-- nav_list -->
     </header>
 <main class="main_wrapper">
     <section class="slider">
-        <h2 class="visually_hidden sub_title">Slider</h2>
+        <h2 class="visually-hidden sub_title">Slider</h2>
         <ul class="slider_list">
             <li class="slider_item"><img src="/images/arch_linux.png" alt="" width="1440" height="900"></li>
         </ul>
@@ -35,12 +33,14 @@
     <!-- slider -->
     <section class="about_project">
         <h2 class="sub_title">О проекте</h2>
-        <div class="project_content">
+        <div class="about_project">
+            <p class="about_project_content">
             Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. Я совсем один и блаженствую в здешнем краю, словно созданном для таких, как я. Я так счастлив, мой друг, так упоен ощущением покоя, что искусство мое страдает от этого. Ни одного штриха не мог бы я сделать, а никогда не был таким большим художником, как в эти минуты. Когда от милой моей долины поднимается пар и полдневное солнце стоит над непроницаемой чащей темного леса и лишь редкий луч проскальзывает в его святая святых, а я лежу в высокой траве у быстрого ручья и, прильнув к земле, вижу тысячи всевозможных былинок и чувствую, как близок моему сердцу крошечный мирок, что снует между стебельками, наблюдаю эти неисчислимые, непостижимые разновидности червяков и мошек и чувствую близость всемогущего, создавшего нас по своему подобию, веяние вселюбящего, судившего нам парить в вечном блаженстве, когда взор мой туманится и все вокруг меня и небо надо мной запечатлены в моей душе, точно образ возлюбленной, - тогда, дорогой друг, меня часто томит мысль: "Ах! Как бы выразить, как бы вдохнуть в рисунок то, что так полно, так трепетно живет во мне, запечатлеть отражение моей души, как душа моя - отражение предвечного бога!" Друг
+            </p>
         </div>
     </section>
     <section class="popular_articles">
-        <h2>Популярные статьи</h2>
+        <h2 class="sub_title" >Популярные статьи</h2>
         <div class="popular_article_wrapper">
             <article class="popular_article">
                 <h3><a href="#">Название стать</a></h3>
@@ -68,11 +68,11 @@
 </main>
 <!-- main_wrapper -->
 <footer class="footer">
-    <div class="logo_wrapper logo_wrapper_footer">
-        <h1><a href="/" class="logo logo_footer">IT<span>blog</span></a></h1>
+    <div class=" logo_wrapper_footer">
+        <h1><a href="/" class="logo logo_footer">IT.blog</a></h1>
     </div>
     <section class="social">
-        <h2 class="visually_hidden">Мы в социальных сетях</h2>
+        <h2 class="visually-hidden">Мы в социальных сетях</h2>
         <ul class="social_list">
             <li class="social_icon">
                 <a href="#">Twitter</a>
@@ -81,10 +81,11 @@
                 <a href="#">Facebook</a>
             </li>
             <li class="social_icon">
-                <a href="#">youtube</a>
+                <a href="#">Youtube</a>
             </li>
         </ul>
     </section>
+    <div class="copyright">&copy; 2020 Denys Skotarenko</div>
 </footer>
 </body>
 </html>
